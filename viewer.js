0c684606b6bfcd1232c30862088e3d8a46087f6b
drakeirving <shinugayoi@gmail.com>
2019-10-07 01:26:18 -0500

    minor changes

--- viewer.js
+++ viewer.js
@@ -223,7 +223,7 @@ function setupControls(){
     }
     drag(e);
     sliderContainer.addEventListener('pointermove', drag);
-    sliderContainer.addEventListener('pointerup', e => {
+    document.addEventListener('pointerup', e => {
       sliderContainer.removeEventListener('pointermove', drag);
     }, {once: true});
   });
