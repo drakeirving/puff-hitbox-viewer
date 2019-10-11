3ca3dc6e841adb8c06644888f22e53e4278f62ca
drakeirving <shinugayoi@gmail.com>
2019-10-11 04:14:45 -0500

    scraper bits

--- scraper.js
+++ scraper.js
@@ -1,101 +1,99 @@
-// Scraping from Ruben's script viewer
-
 class Moveset extends Map {
-	constructor(){ super(); }
+  constructor(){ super(); }
 
   addCurrentMove(niceName){
     return this.set(
       document.querySelector(".script-select select:last-child").selectedOptions[0].textContent,
       this.scrapeMove(niceName)
     );
-  }
 
-  scrapeMove(niceName){
-    let move = {};
-    move.niceName = niceName;
-    let hitboxes = [];
-    let f = 1;
-    let script = [...document.querySelectorAll(".script-cmd")].map(e=>e.parentNode.textContent);
-    for(let s of script){
-      // Frame
-      let frame = s.match(/^frame\(Frame=(\d+)\)/);
-      if(frame !== null && frame.length==2){
-        f = Number(frame[1]);
-        continue;
+    scrapeMove(niceName){
+      let move = {};
+      move.niceName = niceName;
+      let hitboxes = [];
+      let f = 1;
+      let script = [...document.querySelectorAll(".script-cmd")].map(e=>e.parentNode.textContent);
+      for(let s of script){
+        // Frame
+        let frame = s.match(/^frame\(Frame=(\d+)\)/);
+        if(frame !== null && frame.length==2){
+          f = Number(frame[1]);
+          continue;
+        }
+        // Wait
+        let wait = s.match(/^wait\(Frames=(\d+)\)/) || s.match(/^wait\(0, (\d+)\)/) ;
+        if(wait !== null && wait.length==2){
+          f += Number(wait[1]);
+          continue;
+        }
+        // Attack
+        if(/^\s*ATTACK\(/.test(s)){
+          let h = readHitbox(s);
+          h._frameStart = f;
+          hitboxes.forEach((h2,i) => {
+            if(!("_frameEnd" in h2) && h.ID == h2.ID){
+              h2._frameEnd = f;
+            }
+          });
+          hitboxes.push(h);
+          continue;
+        }
+        // Grab
+        if(/^\s*CATCH\(/.test(s)){
+          let h = readHitbox(s);
+          h._frameStart = f;
+          hitboxes.forEach((h2,i) => {
+            if(!("_frameEnd" in h2) && h.ID == h2.ID){
+              h2._frameEnd = f;
+            }
+          });
+          hitboxes.push(h);
+          continue;
+        }
+        // Clear all hitboxes
+        if(/^\s*AttackModule__clear_all/.test(s) || /^\s*grab\(MA_MSC_CMD_GRAB_CLEAR_ALL\)/.test(s)){
+          hitboxes.forEach(h => {
+            if(!("_frameEnd" in h)){
+              h._frameEnd = f;
+            }
+          });
+          continue;
+        }
+        // Clear hitbox ID
+        let clear = s.match(/^\s*AttackModule__clear\(ID=(\d+)\)/);
+        if(clear !== null && clear.length==2){
+          hitboxes.forEach(h=>{if(h.ID == clear[1] && !("_frameEnd" in h)){h._frameEnd = f;}});
+          continue;
+        }
+        // Autocancel
+        if(/^\s*WorkModule__off_flag\(Flag=FIGHTER_STATUS_ATTACK_AIR_FLAG_ENABLE_LANDING\)/.test(s)){
+          move.autocancel = f;
+        }
+        // FT_MOTION_RATE
+        if(/^FT_MOTION_RATE/.test(s)){
+          let msg = `WARNING (${niceName}): FT_MOTION_RATE is used. Please adjust frames manually.`;
+          console.warn(msg); alert(msg);
+          continue;
+        }
+        // For-loop
+        if(/^for\(/.test(s)){
+          let msg = `WARNING (${niceName}): for loop is used. Please adjust frames manually.`;
+          console.warn(msg); alert(msg);
+          continue;
+        }
       }
-      // Wait
-      let wait = s.match(/^wait\(Frames=(\d+)\)/) || s.match(/^wait\(0, (\d+)\)/) ;
-      if(wait !== null && wait.length==2){
-        f += Number(wait[1]);
-        continue;
-      }
-      // Attack
-      if(/^\s*ATTACK\(/.test(s)){
-        let h = readHitbox(s);
-        h._frameStart = f;
-        hitboxes.forEach((h2,i) => {
-          if(!("_frameEnd" in h2) && h.ID == h2.ID){
-            h2._frameEnd = f;
-          }
-        });
-        hitboxes.push(h);
-        continue;
-      }
-      // Grab
-      if(/^\s*CATCH\(/.test(s)){
-        let h = readHitbox(s);
-        h._frameStart = f;
-        hitboxes.forEach((h2,i) => {
-          if(!("_frameEnd" in h2) && h.ID == h2.ID){
-            h2._frameEnd = f;
-          }
-        });
-        hitboxes.push(h);
-        continue;
-      }
-      // Clear all hitboxes
-      if(/^\s*AttackModule__clear_all/.test(s) || /^\s*grab\(MA_MSC_CMD_GRAB_CLEAR_ALL\)/.test(s)){
-        hitboxes.forEach(h => {
-          if(!("_frameEnd" in h)){
-            h._frameEnd = f;
-          }
-        });
-        continue;
-      }
-      // Clear hitbox ID
-      let clear = s.match(/^\s*AttackModule__clear\(ID=(\d+)\)/);
-      if(clear !== null && clear.length==2){
-        hitboxes.forEach(h=>{if(h.ID == clear[1] && !("_frameEnd" in h)){h._frameEnd = f;}});
-        continue;
-      }
-      // Autocancel
-      if(/^\s*WorkModule__off_flag\(Flag=FIGHTER_STATUS_ATTACK_AIR_FLAG_ENABLE_LANDING\)/.test(s)){
-        move.autocancel = f;
-      }
-      // FT_MOTION_RATE
-      if(/^FT_MOTION_RATE/.test(s)){
-        let msg = `WARNING (${niceName}): FT_MOTION_RATE is used. Please adjust frames manually.`;
-        console.warn(msg); alert(msg);
-        continue;
-      }
-      // For-loop
-      if(/^for\(/.test(s)){
-        let msg = `WARNING (${niceName}): for loop is used. Please adjust frames manually.`;
-        console.warn(msg); alert(msg);
-        continue;
-      }
-    }
-    hitboxes.forEach(h=>{if(!("_frameEnd" in h)){h._frameEnd = f;}});
-    move.hitboxes = hitboxes;
-    return move;
+      hitboxes.forEach(h=>{if(!("_frameEnd" in h)){h._frameEnd = f;}});
+      move.hitboxes = hitboxes;
+      return move;
 
-    function readHitbox(s){
-      let h = Object.fromEntries(
-          [...s.matchAll(/([\w\/]+)=([\w\._]+)/g)]
-          .map(x => [x[1], x[2]])
-      );
-      if(s.includes("CATCH(")){ h._type = "Grab"; }
-      return h;
+      function readHitbox(s){
+        let h = Object.fromEntries(
+            [...s.matchAll(/([\w\/]+)=([\w\._]+)/g)]
+            .map(x => [x[1], x[2]])
+        );
+        if(s.includes("CATCH(")){ h._type = "Grab"; }
+        return h;
+      }
     }
   }
 
@@ -125,19 +123,12 @@ class Moveset extends Map {
   }
 }
 
-// utility functions
+// helper functions
 
-function changeScript(index){
-  let select = document.querySelector(".script-select select:last-child")
-  select.value = index;
-  let e = document.createEvent("HTMLEvents");
-  e.initEvent("change", true, true);
-  select.dispatchEvent(e);
-}
-
-function addScript(moveset, index, niceName){
-  changeScript(index);
-  return moveset.addCurrentMove(niceName);
+function createMoveset(scriptMap){
+  let moveset = new Moveset();
+  addAllScripts(moveset, scriptMap);
+  return moveset;
 }
 
 function addAllScripts(moveset, scriptMap){
@@ -145,6 +136,19 @@ function addAllScripts(moveset, scriptMap){
     addScript(moveset, i, scriptMap[i])
   });
   return moveset;
+}
+
+function addScript(moveset, index, niceName){
+  changeScript(index);
+  return moveset.addCurrentMove(niceName);
+
+  function changeScript(index){
+    let select = document.querySelector(".script-select select:last-child")
+    select.value = index;
+    let e = document.createEvent("HTMLEvents");
+    e.initEvent("change", true, true);
+    select.dispatchEvent(e);
+  }
 }
 
 function saveMoveset(moveset, filename="moveset.json"){
@@ -155,14 +159,11 @@ function saveMoveset(moveset, filename="moveset.json"){
   a.dispatchEvent(new MouseEvent("click"));
 }
 
-(function example_run(){
-  let moveset = new Moveset();
-  let scriptMap = {
-    26: "Neutral Air",
-    27: "Forward Air",
-    29: "Up Air"
-  };
-  addAllScripts(moveset, scriptMap);
-  // saveMoveset(moveset);
-	return moveset;
-})();
+// (function example_run(){
+//   let scriptMap = {
+//     15: "Jab 1",
+//     16: "Jab 2",
+//     17: "Dash Attack"
+//   };
+//   return createMoveset(scriptMap);
+// })();
